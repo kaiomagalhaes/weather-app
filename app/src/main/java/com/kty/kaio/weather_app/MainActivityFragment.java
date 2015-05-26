786a0526a64a4ca98bb076a90c2fb4cc9447d4b4
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-05-25 23:42:07 -0300

    udacity lesson 1 complet

--- app/src/main/java/com/kty/kaio/weather_app/MainActivityFragment.java
+++ app/src/main/java/com/kty/kaio/weather_app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -12,12 +17,32 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter<String> arrayAdapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> weekForecast = new ArrayList<String>() {{
+            add("Peepa pig");
+            add("Tomorrow");
+            add("Weds");
+            add("Thurs");
+            add("Fri");
+            add("Foggy");
+            add("Sunny");
+        }};
+
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(arrayAdapter);
+
+        return rootView;
     }
 }
